26c7138a92506c7ea9a74992ad1dc2397b820d8b
John Mark E. Flameno <johnmarkflameno@gmail.com>
2025-02-06 18:03:42 +0800

    Fixed the server problem

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		host: '0.0.0.0', // Ensures the app is accessible externally
+		allowedHosts: [
+			'momentum-tvck.onrender.com', // Add your Render domain here
+		],
+	},
+});
